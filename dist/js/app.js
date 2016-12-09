bdae2a9a08c3df8369e35fc94278aeb7fb5e7107
Sally Northmore <sally.northmore@gmail.com>
2016-12-09 16:13:26 +0000

    Sets up NPM scripts for CSS and Nunjucks templating system

---
+++ dist/js/app.js
@@ -0,0 +1,17 @@
+var app = {
+
+		'init': function init() {},
+
+		'orientation': function orientation() {}
+};
+
+/**
+ * START POINT
+*/
+
+$(document).ready(function () {
+
+		app.init();
+		console.log("%c   ***** Loaded ******   ", 'background-color: #0f0; color: #fff;');
+});
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uLy4uL3NyYy9hcHAuanMiXSwibmFtZXMiOlsiYXBwIiwiaW5pdCIsIm9yaWVudGF0aW9uIiwiJCIsImRvY3VtZW50IiwicmVhZHkiLCJjb25zb2xlIiwibG9nIl0sIm1hcHBpbmdzIjoiQUFBQSxJQUFJQSxNQUFNOztBQUVSLFVBQVEsU0FBU0MsSUFBVCxHQUFnQixDQUV2QixDQUpPOztBQU9SLGlCQUFlLFNBQVNDLFdBQVQsR0FBdUIsQ0FFckM7QUFUTyxDQUFWOztBQWFBOzs7O0FBSUFDLEVBQUVDLFFBQUYsRUFBWUMsS0FBWixDQUFrQixZQUFXOztBQUUzQkwsTUFBSUMsSUFBSjtBQUNBSyxVQUFRQyxHQUFSLENBQVksNkJBQVosRUFBMkMsc0NBQTNDO0FBQ0QsQ0FKRCIsImZpbGUiOiJhcHAuanMiLCJzb3VyY2VzQ29udGVudCI6WyJ2YXIgYXBwID0ge1xuXG5cdFx0J2luaXQnOiBmdW5jdGlvbiBpbml0KCkge1xuXG5cdFx0fSxcblxuXG5cdFx0J29yaWVudGF0aW9uJzogZnVuY3Rpb24gb3JpZW50YXRpb24oKSB7XG5cblx0XHR9LFxufTtcblxuXG4vKipcbiAqIFNUQVJUIFBPSU5UXG4qL1xuXG4kKGRvY3VtZW50KS5yZWFkeShmdW5jdGlvbigpIHtcblxuXHRcdGFwcC5pbml0KCk7XG5cdFx0Y29uc29sZS5sb2coXCIlYyAgICoqKioqIExvYWRlZCAqKioqKiogICBcIiwgJ2JhY2tncm91bmQtY29sb3I6ICMwZjA7IGNvbG9yOiAjZmZmOycpO1xufSk7XG4iXX0=
---
+++ src/app.js
@@ -0,0 +1,22 @@
+var app = {
+
+		'init': function init() {
+
+		},
+
+
+		'orientation': function orientation() {
+
+		},
+};
+
+
+/**
+ * START POINT
+*/
+
+$(document).ready(function() {
+
+		app.init();
+		console.log("%c   ***** Loaded ******   ", 'background-color: #0f0; color: #fff;');
+});
